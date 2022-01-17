6cdfaed5a76cc2c8dfb87d223d0b85a69ab7e258
mihaelniko <mihaelbutrakov@gmail.com>
2022-01-17 16:45:14 +0200

    Added down movement

--- Scripts/main.py
+++ Scripts/main.py
@@ -57,6 +57,8 @@ while True:
 
     if enemy_array[len(enemy_array) - 1].x >= SCREEN_SIZE[0] - 30 or enemy_array[0].x < 0:
         enemy_speed *= -1
+        for enemy in enemy_array:
+            enemy.y += 10
 
     for enemy_rect in enemy_array:
         pygame.draw.rect(screen, (0, 0, 255), enemy_rect, 2, 3)
